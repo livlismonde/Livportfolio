29c665e3b8bb5b60fc3687c79603d7a882724e0f
livlismonde <77112594+livlismonde@users.noreply.github.com>
2022-01-18 19:10:21 +0100

    finishing touches

--- fake_scrolling.js
+++ fake_scrolling.js
@@ -5,22 +5,22 @@ function onScroll() {
   const pixelsToScroll = documentHeight - windowHeight;
 
   // Scroll column1 together with the window
-  const column1 = document.querySelector("#column-1");
+  const column1 = document.querySelector(".column-1");
   column1.style.transform = `translate(0, ${
     -pixelsToScroll * scrollPercentage
   }px)`;
 
-  const column2 = document.querySelector("#column-2");
+  const column2 = document.querySelector(".column-2");
   column2.style.transform = `translate(0, ${
     -pixelsToScroll * (1 - scrollPercentage)
   }px)`;
 
-  const column3 = document.querySelector("#column-3");
+  const column3 = document.querySelector(".column-3");
   column3.style.transform = `translate(0, ${
     -pixelsToScroll * scrollPercentage
   }px)`;
 
-  const column4 = document.querySelector("#column-4");
+  const column4 = document.querySelector(".column-4");
   column4.style.transform = `translate(0, ${
     -pixelsToScroll * (1 - scrollPercentage)
   }px)`;
@@ -30,12 +30,3 @@ if (window.innerWidth >= 601) {
   window.addEventListener("resize", onScroll);
   document.addEventListener("DOMcontentLoaded", onScroll);
 }
-
-document.querySelector("#menu-close").addEventListener("click", function () {
-  document.querySelector(".mobile-menu").style.display = "none";
-});
-document
-  .querySelector("#mobile-menu-hamburger")
-  .addEventListener("click", function () {
-    document.querySelector(".mobile-menu").style.display = "flex";
-  });
